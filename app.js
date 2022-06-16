6e0f69d45c589f672d8e9008ad4e9bea18cfe86d
Jose David <josedbolivarmafrontend@gmail.com>
2022-06-16 13:23:01 -0500

    Add Template

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Servir contenido estático
+
+app.use( express.static('public') );
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/generic.html', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+})
+
+app.get('/elements.html', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
