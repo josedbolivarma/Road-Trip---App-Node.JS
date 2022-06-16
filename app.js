6e3a1d6d0286d2bea8d07c9ddaabd37c4aa9b650
Jose David <josedbolivarmafrontend@gmail.com>
2022-06-16 14:50:41 -0500

    Add Handlebars hbs

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 const port = 8080;
 
-// Servir contenido estático
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
+// Servir contenido estático
 app.use( express.static('public') );
 
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// })
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('home', {
+        nombre: 'José Bolivar',
+        titulo: 'Página estática'
+    });
 })
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
-})
+        res.render('generic', {
+            nombre: 'Jose Bolivar',
+            titulo: 'Página estática'
+        });
+    })
 
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
-})
+    app.get('/elements', (req, res) => {
+        res.render('elements', {
+            nombre: 'Jose Bolivar',
+            titulo: 'Página estática'
+        });
+    })
+
+// app.get('/generic', (req, res) => {
+//     res.sendFile(__dirname + '/public/generic.html');
+// })
+
+// app.get('/elements', (req, res) => {
+//     res.sendFile(__dirname + '/public/elements.html');
+// })
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
